702b1b507efd83be964335a651f6f3233cdb19c6
qccuongg <tqcwww@gmail.com>
2024-04-20 10:47:24 +0700

    Lab1

--- Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Lab1Q2.java
+++ Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Lab1Q2.java
@@ -11,12 +11,19 @@ package com.mycompany.lab1.q2;
 public class Lab1Q2 {
 
     public static void main(String[] args) {
+        //Q1
         //System.out.print(new Question1(123456).SumFirstLastDigit());
         
+        //Q2
         //System.out.print(new Question2 (12, 23, 14).Minimum());
         
+        //Q3
         //System.out.print(new Question3 (5).Hailstone());
         
+        //Q4
         //System.out.print(new Question4 (5).SumEven());
+        
+        //Q5
+        
     }
 }
---
+++ Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Question5.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab1.q2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class Question5 {
+    ArrayList<Integer> ArrList = new ArrayList<>();
+    
+    public void RemoveFirst (ArrList) {
+        
+}
