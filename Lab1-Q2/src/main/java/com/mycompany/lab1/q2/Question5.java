6987520d26c08936c43a3989bc35975d775fc2e4
qccuongg <tqcwww@gmail.com>
2024-04-20 10:59:22 +0700

    v10

--- Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Question5.java
+++ Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Question5.java
@@ -5,14 +5,67 @@
 package com.mycompany.lab1.q2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 
 /**
  *
  * @author Student
  */
 public class Question5 {
-    ArrayList<Integer> ArrList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Integer> ArrList;
     
-    public void RemoveFirst (ArrList) {
-        
+    public ArrayList<Integer> getList() {
+        return ArrList;
+    }
+       
+    public Question5(){
+        this.ArrList = new ArrayList<>();
+    }
+    
+    public Question5(ArrayList<Integer> ArrList){
+        this.ArrList = ArrList;
+    }
+    
+    public void CreateList () {
+        System.out.print("Enter the length of the list: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the elements of the list: ");
+        for (int i = 0; i < n; i++)
+            ArrList.add(sc.nextInt());
+    }
+    
+    public void RemoveFirst () {
+        System.out.print("Enter the number you want to delete: ");
+        int a = sc.nextInt();
+        for (int i = 0; i < ArrList.size(); i++) {
+            if (ArrList.get(i) == a){
+                ArrList.remove(i);
+                break;
+            }
+        }
+    }
+    
+    public void ReplaceElement (int pos, int num) {
+        ArrList.remove(ArrList.size() - 1);
+        ArrList.set(pos, num);
+    }
+    
+    public void DuplicateFinder() {
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicate = new HashSet<>();
+        for (Integer number : ArrList) {
+            if (!seen.add(number)) {
+                duplicate.add(number);
+            }
+        }
+        System.out.println("[" + duplicate + "]");
+    }
+    
+    public void DuplicateDelete() {
+        HashSet<Integer> seen = new HashSet<>();
+        ArrList.removeIf(i -> !seen.add(i));
+        System.out.println(ArrList);
+    }
 }
--- Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Question6.java
+++ Lab1-Q2/src/main/java/com/mycompany/lab1/q2/Question6.java
@@ -4,10 +4,60 @@
  */
 package com.mycompany.lab1.q2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author Student
  */
 public class Question6 {
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
     
+    public Question6(){}
+    
+    public Question6(String str){
+        this.str = str;
+    }
+    
+    public void StrLength(){
+        System.out.println(str.length());
+    }
+    
+    public void WordCount(){
+        int wordnum = 1;
+        for (char c : str.toCharArray()) {
+            if (c != ' '){
+                wordnum++;
+            }
+        }
+        System.out.println(wordnum);
+    }
+    
+    public void Concatenate(String a){
+        System.out.println(str.concat(a));
+    }
+    
+    public void Palindrome(){
+        ArrayList<Character> CharList = new ArrayList<Character>();
+        for (char c : str.toCharArray()) {
+            CharList.add(c);
+        }
+        ArrayList<Character> DuplicatedList = new ArrayList<Character>(CharList);
+        Collections.reverse(CharList);
+        if (CharList.equals(DuplicatedList)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
 }
